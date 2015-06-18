fa89178c1cf716638f27b6b3becf24dea1a0c49e
kittimiyo <kittimiyo@hotmail.com>
2015-06-17 23:51:58 -0700

    made more good

--- scripts/sudokuScript.js
+++ scripts/sudokuScript.js
@@ -2,12 +2,24 @@
  * Created by boenkes on 1/9/2015.
  */
 
-var a = [[[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]],[[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]],[[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]]];
+var cell = {
+    hint: [1,2,3,4,5,6,7,8,9],
+    value: '',
+    position: -1
+}
+
+var a = [[[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]] ],
+        [ [[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]] ],
+        [ [[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]]];
 for (var i=0;i<3;i++) {
     for(var j=0;j<3;j++) {
         for (var k=0;k<3;k++) {
             for (var l=0;l<3;l++) {
-                a[i][j][k][l] = i+j+k+l;
+                a[i][j][k][l] = {
+                    hint: [1,2,3,4,5,6,7,8,9],
+                    value: null,
+                    position: i+j+k+l
+                }
             }
         }
     }
@@ -34,18 +46,16 @@ var print = function()
                         val = inputs[ind].value;
                         if ((val != 1) && (val != 2) && (val != 3) && (val != 4) && (val != 5) && (val != 6) && (val != 7) && (val != 8) && (val != 9) && (val != ""))
                         {
-                            err += "<br>Invalid input at row " + ((i*3)+(k+1)) + ", column " + ((j*3)+(l+1)) +": [" + inputs[ind].value +"]";
+                            err += "<br>Invalid input at row " + ((i*3)+(k+1)) + ", column " + ((j*3)+(l+1)) +": [" + val +"]";
                             valid = false;
+                            val = null;
                         }
-                        else if (val == '')
+                        else if (val !== '')
                         {
-                            st += inputs[ind].value;
-                        }
-                        else 
-                        {
-                            st += "<br>" + inputs[ind].value;
+                            st += "<br>" + val;
                             numberFound = true;
                         }
+                        a[i][j][k][l].value = (val === ('' || null) ? null : Number(val));
                     }
                     else st += 'emp';
                     st+= ' ';
