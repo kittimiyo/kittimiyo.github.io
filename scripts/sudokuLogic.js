1095782bf149b4498b989e70b36cc4fe355aa2ab
Michael Nauman and Sylvie Boenke-Bowden <pair+michael.nauman@autodesk.com+sylvie.boenke-bowden@autodesk.com@autodesk.com>
2015-06-18 09:48:55 -0700

    started logic file, loading values correctly

---
+++ scripts/sudokuLogic.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Sylvie on 6/18/15.
+ */
+
+test = function() {
+  console.log('test passed');
+};
--- scripts/sudokuScript.js
+++ scripts/sudokuScript.js
@@ -2,15 +2,12 @@
  * Created by boenkes on 1/9/2015.
  */
 
-var cell = {
-    hint: [1,2,3,4,5,6,7,8,9],
-    value: '',
-    position: -1
-}
+//var logic = require('./sudokuLogic')  //todo: requirejs
 
 var a = [[[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]] ],
         [ [[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]] ],
         [ [[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]]];
+
 for (var i=0;i<3;i++) {
     for(var j=0;j<3;j++) {
         for (var k=0;k<3;k++) {
@@ -18,7 +15,7 @@ for (var i=0;i<3;i++) {
                 a[i][j][k][l] = {
                     hint: [1,2,3,4,5,6,7,8,9],
                     value: null,
-                    position: i+j+k+l
+                    index: (27*i) + (9*j) + (3*k) + l
                 }
             }
         }
@@ -44,18 +41,23 @@ var print = function()
                     if (inputs[ind].value != null)
                     {
                         val = inputs[ind].value;
-                        if ((val != 1) && (val != 2) && (val != 3) && (val != 4) && (val != 5) && (val != 6) && (val != 7) && (val != 8) && (val != 9) && (val != ""))
+                        if ((val !== '1') && (val !== '2') && (val !== '3')
+                          && (val !== '4') && (val !== '5') && (val !== '6')
+                          && (val !== '7') && (val !== '8') && (val !== '9'))
                         {
-                            err += "<br>Invalid input at row " + ((i*3)+(k+1)) + ", column " + ((j*3)+(l+1)) +": [" + val +"]";
-                            valid = false;
+                            if (val !== '') {
+                                err += "<br>Invalid input at row " + ((i*3)+(k+1)) + ", column " + ((j*3)+(l+1)) +": [" + val +"]";
+                                valid = false;
+                            }
+
                             val = null;
                         }
-                        else if (val !== '')
+                        else
                         {
                             st += "<br>" + val;
                             numberFound = true;
                         }
-                        a[i][j][k][l].value = (val === ('' || null) ? null : Number(val));
+                        a[i][j][k][l].value = (val === null ? null : Number(val));
                     }
                     else st += 'emp';
                     st+= ' ';
@@ -77,5 +79,6 @@ var print = function()
     document.getElementById("msg").innerHTML = err;
     numberFound = false;
     console.log(st);
-}
+  test();
+};
 
