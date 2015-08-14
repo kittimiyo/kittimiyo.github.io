67ca4a6001f49ccb8104abf6b72b96297814e529
kittimiyo <kittimiyo@hotmail.com>
2015-08-13 17:53:04 -0700

    testing map

--- sudoku_backbone/data/data.js
+++ sudoku_backbone/data/data.js
@@ -2,44 +2,98 @@
  * Created by sylvie on 8/12/15.
  */
 
-var index = [], sudokuData2 = [];
-
-index[2] = 1;
-index[5] = 6;
-index[7] = 4;
-index[12] = 3;
-index[14] = 9;
-index[17] = 7;
-index[19] = 2;
-index[22] = 4;
-index[23] = 8;
-index[25] = 1;
-index[31] = 6;
-index[32] = 4;
-index[34] = 2;
-index[35] = 8;
-index[36] = 4;
-index[39] = 1;
-index[41] = 2;
-index[44] = 5;
-index[45] = 5;
-index[46] = 3;
-index[48] = 8;
-index[49] = 9;
-index[55] = 1;
-index[57] = 6;
-index[58] = 9;
-index[61] = 3;
-index[63] = 7;
-index[66] = 2;
-index[68] = 8;
-index[73] = 5;
-index[75] = 1;
-index[78] = 2;
+var sudokuBlank = [], sudokuData = [];
 
 for (var i = 0; i < 81; i++) {
   sudokuData2.push({
-    value: index[i] ? index[i] : 0,
+    value: sudokuEntries2[i] ? sudokuEntries2[i] : 0,
+    index: i
+  });
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var sudokuEntries1 = [], sudokuData1 = [];
+
+sudokuEntries2[2] = 1;
+sudokuEntries2[5] = 6;
+sudokuEntries2[7] = 4;
+sudokuEntries2[12] = 3;
+sudokuEntries2[14] = 9;
+sudokuEntries2[17] = 7;
+sudokuEntries2[19] = 2;
+sudokuEntries2[22] = 4;
+sudokuEntries2[23] = 8;
+sudokuEntries2[25] = 1;
+sudokuEntries2[31] = 6;
+sudokuEntries2[32] = 4;
+sudokuEntries2[34] = 2;
+sudokuEntries2[35] = 8;
+sudokuEntries2[36] = 4;
+sudokuEntries2[39] = 1;
+sudokuEntries2[41] = 2;
+sudokuEntries2[44] = 5;
+sudokuEntries2[45] = 5;
+sudokuEntries2[46] = 3;
+sudokuEntries2[48] = 8;
+sudokuEntries2[49] = 9;
+sudokuEntries2[55] = 1;
+sudokuEntries2[57] = 6;
+sudokuEntries2[58] = 9;
+sudokuEntries2[61] = 3;
+sudokuEntries2[63] = 7;
+sudokuEntries2[66] = 2;
+sudokuEntries2[68] = 8;
+sudokuEntries2[73] = 5;
+sudokuEntries2[75] = 1;
+sudokuEntries2[78] = 2;
+
+for (var i = 0; i < 81; i++) {
+  sudokuData2.push({
+    value: sudokuEntries2[i] ? sudokuEntries2[i] : 0,
+    index: i
+  });
+}
+
+
+var sudokuEntries2 = [], sudokuData2 = [];
+
+sudokuEntries2[2] = 1;
+sudokuEntries2[5] = 6;
+sudokuEntries2[7] = 4;
+sudokuEntries2[12] = 3;
+sudokuEntries2[14] = 9;
+sudokuEntries2[17] = 7;
+sudokuEntries2[19] = 2;
+sudokuEntries2[22] = 4;
+sudokuEntries2[23] = 8;
+sudokuEntries2[25] = 1;
+sudokuEntries2[31] = 6;
+sudokuEntries2[32] = 4;
+sudokuEntries2[34] = 2;
+sudokuEntries2[35] = 8;
+sudokuEntries2[36] = 4;
+sudokuEntries2[39] = 1;
+sudokuEntries2[41] = 2;
+sudokuEntries2[44] = 5;
+sudokuEntries2[45] = 5;
+sudokuEntries2[46] = 3;
+sudokuEntries2[48] = 8;
+sudokuEntries2[49] = 9;
+sudokuEntries2[55] = 1;
+sudokuEntries2[57] = 6;
+sudokuEntries2[58] = 9;
+sudokuEntries2[61] = 3;
+sudokuEntries2[63] = 7;
+sudokuEntries2[66] = 2;
+sudokuEntries2[68] = 8;
+sudokuEntries2[73] = 5;
+sudokuEntries2[75] = 1;
+sudokuEntries2[78] = 2;
+
+for (var i = 0; i < 81; i++) {
+  sudokuData2.push({
+    value: sudokuEntries2[i] ? sudokuEntries2[i] : 0,
     index: i
   });
 }
