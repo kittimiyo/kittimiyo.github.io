5094522969b1825e5b32f62289128b67fd5f1829
Sylvie Boenke-Bowden <sylvie.boenke-bowden@autodesk.com>
2017-02-23 09:53:23 -0800

    updated a bunch of fonts

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -6,9 +6,9 @@ const Header = React.createClass({
   render: function() {
     return (
       <div id="header">
-        <div className="names">Eric & Sylvie</div>
-        <div className="join">Join us</div>
+        <div className="names">Sylvie & Eric</div>
         <div className="date">June 24, 2017</div>
+        <div className="location">Santa Rosa, CA</div>
       </div>
     );
   }
