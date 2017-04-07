886ed53d2a32225f0704f9e63ba4eb2aa2e8b552
boenkes <sylvie.boenke-bowden@autodesk.com>
2017-04-07 10:49:14 -0700

    updated location!

--- app/components/container-location.jsx
+++ app/components/container-location.jsx
@@ -1,21 +1,86 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import Iframe from 'react-iframe';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
+//import { GoogleApiWrapper } from 'GoogleMapsReactComponent';
 
 import '../styles/container-location.scss'
 
-function LocationContainer() {
-  if(this.props.NavigatorStore.selected === 'location'){
-    return (
-      <div id="container-location" className="container">
-        <div className="title">Location</div>
-        <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3122.77047034644!2d-122.7514826846615!3d38.49293797963511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808438c59cd66501%3A0x997384afcf9a4ae8!2sLuther+Burbank+Center+for+the+Arts!5e0!3m2!1sen!2sus!4v1490737665781" width="600px" height="450px" frameborder="0" style={{border:0}} allowfullscreen></Iframe>
-      </div>
-    );
+class LocationContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfoWindow: false,
+      selectedPlace: {},
+      activeMarker: {},
+      directions: 'https://www.google.com/maps/dir//Luther+Burbank+Center+for+the+Arts,+Mark+West+Springs+Rd,+Santa+Rosa,+CA/@38.4929328,-122.7843132,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x808438c59cd66501:0x997384afcf9a4ae8!2m2!1d-122.749294!2d38.492938'
+    };
+
+    this.onMarkerClick = this.onMarkerClick.bind(this);
   }
 
-  return null;
+  onMarkerClick(props, marker) {
+    if (this.state.showInfoWindow) {
+      this.setState({
+        showInfoWindow: false
+      });
+    } else {
+      this.setState({
+        showInfoWindow: true,
+        selectedPlace: props.name,
+        activeMarker: marker
+      });
+    }
+  }
+
+  render() {
+    if(this.props.NavigatorStore.selected === 'location'){
+      return (
+        <div id="container-location" className="container">
+          <div className="title">Location</div>
+          <div className='info'><strong>Luther Burbank Center for the Arts, Carston Cabaret Room<br/></strong>
+            50 Mark West Springs Road,
+            Santa Rosa, CA 95403</div>
+
+          <Map
+            style={{ borderRadius: '4px' }}
+            containerStyle={{width: '80%', height: '450px', position: 'relative'}}
+            className="google-map"
+            initialCenter={{lat: 38.49293797963511, lng: -122.7514826846615}}
+            google={this.props.google}
+            zoom={13}>
+
+            <Marker
+              name={'Sylvie & Eric Wedding Location'}
+              onClick={this.onMarkerClick}
+              position={{lat: 38.49293797963511, lng: -122.7514826846615}} />
+
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showInfoWindow}>
+              <div className="info-window">
+                <div className="name">Sylvie & Eric Wedding Location</div>
+                <div className="center-name">Luther Burbank Center for the Arts</div>
+                <a
+                  className="directions"
+                  href={this.state.directions}
+                  target="_blank"
+                >Get Directions</a>
+              </div>
+            </InfoWindow>
+
+          </Map>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+
 }
 
 
-export default inject('NavigatorStore')(observer(LocationContainer));
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBvhEx4PWpKihF7oEpUxRh_MjmQ542AzM8'
+})(inject('NavigatorStore')(observer(LocationContainer)));
--- app/components/side-menu.jsx
+++ app/components/side-menu.jsx
@@ -23,29 +23,33 @@ class SideMenu extends React.Component {
   render() {
     return (
       <div id="side-menu" >
-        <div className="side-menu-center">
-        <div
-          id="rsvp"
-          className={this.state.selected === 'rsvp' ? 'selected' : ''}
-          onClick={this.select}>RSVP</div>
-        <div
-          id="location"
-          className={this.state.selected === 'location' ? 'selected' : ''}
-          onClick={this.select}>LOCATION</div>
-          { /*<span className="location">updated!</span>*/ }
-        <div
-          id="schedule"
-          className={this.state.selected === 'schedule' ? 'selected' : ''}
-          onClick={this.select}>SCHEDULE</div>
-          { /*<span className="schedule">updated!</span>*/ }
-        <a
-          id="registry"
-          className={this.state.selected === 'registry' ? 'selected' : ''}
-          href="http://www.honeyfund.com/wedding/BowdenPetersen2017"
-          target="_blank"
-          className="honeyfund">REGISTRY</a>
+        <div className="background-container">
+          <div className="background"></div>
         </div>
-        <div className="updated registry">updated!</div>
+        <div className="side-menu-center">
+          <div
+            id="rsvp"
+            className={this.state.selected === 'rsvp' ? 'selected' : ''}
+            onClick={this.select}>RSVP</div>
+          <div
+            id="location"
+            className={this.state.selected === 'location' ? 'selected' : ''}
+            onClick={this.select}>LOCATION</div>
+          <div
+            id="schedule"
+            className={this.state.selected === 'schedule' ? 'selected' : ''}
+            onClick={this.select}>SCHEDULE</div>
+          <a
+            id="registry"
+            className={this.state.selected === 'registry' ? 'selected' : ''}
+            href="http://www.honeyfund.com/wedding/BowdenPetersen2017"
+            target="_blank"
+            className="honeyfund">REGISTRY</a>
+        </div>
+        <div className="updated">
+          <div className="location"></div>
+          <div className="registry">updated!</div>
+        </div>
       </div>
     );
   }
